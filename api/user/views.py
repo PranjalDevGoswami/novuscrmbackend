16c4f40debda573f06fc978191bbabe5110c5381
pranjaldev <pranjaldev.goswami@novusinsights.com>
2024-03-20 11:54:54 +0530

    DRF

--- api/user/views.py
+++ api/user/views.py
@@ -85,7 +85,6 @@ class UserLoginViewSet(viewsets.ModelViewSet):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.validated_data['user']
-
         if user is None:
             return Response({
                 'status':400,
@@ -101,7 +100,6 @@ class UserLoginViewSet(viewsets.ModelViewSet):
             })
         
         refresh = RefreshToken.for_user(user)
-        
         return Response({'refresh':str(refresh),'access': str(refresh.access_token), 'success': "Login successfully."})
   
 
